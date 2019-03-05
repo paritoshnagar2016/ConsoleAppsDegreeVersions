2f5dbe809b5071c1cf5e6f282dc406c205314bb2
Paritosh Nagar <paritoshnagar2016@gmail.com>
2019-03-04 19:17:33 -0600

    Removed comments

--- ConsoleAppsDegreeVersions/Program.cs
+++ ConsoleAppsDegreeVersions/Program.cs
@@ -6,13 +6,6 @@ namespace ConsoleAppsDegreeVersions
     {
         static void Main(string[] args)
         {
-            //Adding
-            //changes done
-            //New Developer has made changes
-            //Anamika made changes
-            //Added by Palaksha
-            //Lets see how it pulls 
-            //Adding by Paritosh-111
             Console.WriteLine("Hello World!");
         }
     }
